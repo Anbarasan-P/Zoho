bbdeb0dfe42d9f2a34656fef7b0b8b868290b308
Anbarasan-P <anbarasanpandikumar@gmail.com>
2025-07-20 22:57:40 +0530

    IsPalindrome Problem Solving

---
+++ DSA/9.IsPalindrome.py
@@ -0,0 +1,9 @@
+class matching():
+    def isPalindrome(self, x):
+        return str(x)==str(x)[::-1]
+
+sol = matching()
+
+print(sol.isPalindrome(121))   # True
+print(sol.isPalindrome(-121))  # False
+print(sol.isPalindrome(10))    # False
